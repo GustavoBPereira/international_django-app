fc47e060f9ed96a20ce36d933cb18ba8abfbe438
Gustavo Brito <britopereiragustavo@gmail.com>
2020-04-21 22:21:22 -0300

    removing form

--- international_app/core/views.py
+++ international_app/core/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-from international_app.core.forms import PostForm
-
 
 def index(request):
-    form = PostForm()
-    return render(request, 'index.html', {form: 'form'})
+    return render(request, 'index.html')
